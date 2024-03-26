2ceeb6b5d80ac7922b323260b075ab0a433ec1b5
tienpm1128 <tien.pham@lifesup.com.vn>
2024-03-27 01:17:40 +0700

    add my page

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -2,10 +2,12 @@
 import { Routes } from '@angular/router';
 import matchingRoute from "./matching/matching.route";
 import supportRoute from "./support/support.route";
+import myRoute from "./my/my.route";
 
 const routes: Routes = [
   matchingRoute,
-  supportRoute
+  supportRoute,
+  myRoute
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
---
+++ src/main/webapp/app/entities/my/my.route.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+import {MyComponent} from "./my.component";
+
+const myRoute: Route = {
+  path: 'my',
+  component: MyComponent,
+  title: 'My.title',
+};
+
+export default myRoute;
